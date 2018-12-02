724198a85903c0675c29d7284a52b30ff7258f2e
Yuxi Gu <yuxigu@YuxideMacBook-Pro.local>
2018-12-01 20:23:11 -0800

    Added backend for adding post

--- api.py
+++ api.py
@@ -49,21 +49,49 @@ def restaurants():
 	RestaurantsInfo = restaurants.find({})
 	return render_template("main.html", RestaurantsInfo=RestaurantsInfo)
 
+
 @app.route("/restaurant_detail/<string:RestaurantName>", methods=["GET", "POST"])
 def restaurant_detail(RestaurantName):
 	RestaurantInfo = restaurants.find_one({"name": RestaurantName})
-	cursor = posts.find({"restaurant": RestaurantName})
+	# cursor = posts.find({"restaurant": RestaurantName})
+	# restaurantPosts = []
+	# for post in cursor:
+	# 	restaurantPosts.append(post)
 	
 	if request.method == 'GET':
+		#store current restaurant in session
+		session['currRestaurant'] = RestaurantName
+
+		cursor = posts.find({"restaurant": RestaurantName})
 		restaurantPosts = []
 		for post in cursor:
 			restaurantPosts.append(post)
+		print(RestaurantInfo)
 		return render_template("restaurant_info.html", RestaurantInfo=RestaurantInfo, posts=restaurantPosts)
+	else:
+		if request.form['submit_btn'] == "add new post":
+			postUserName = session['userName']
+			thisRestaurant = session['currRestaurant']
+			time = request.form['postTime']
+
+			newID = postUserName + " " + thisRestaurant + " " + time
+			newPost = {"sender":postUserName, "restaurant": thisRestaurant, "time":time, "respondedPeople":[], "id": newID}
+			posts.insert_one(newPost)
+
+			#search posts related to current restaurant after inserting a new post
+			cursor = posts.find({"restaurant": thisRestaurant})
+			restaurantPosts = []
+			for post in cursor:
+				restaurantPosts.append(post)
+
+			return render_template("restaurant_info.html", RestaurantInfo=RestaurantInfo, posts=restaurantPosts)
+
 
 
 @app.route("/profile/<string:userName>", methods=["GET"])
 def profile(userName):
 	return render_template("profile.html", userName=userName)
+
 
 @app.route("/preferences/<string:userName>", methods=["GET", "POST"])
 def preferences(userName):
@@ -123,6 +151,29 @@ def preference_read_only(userName):
 @app.route("/messages", methods=["GET", "POST"])
 def messages():
 	return render_template("user_profile_msgbox.html")
+
+
+@app.route("/messages", methods=["GET", "POST"])
+#a user send a new response to a post
+def addNewResponseToPost(postId, responseUserName, sentMsg):
+	responses = posts.find_one({"id": postId})["respondedPeople"]
+	if responseUserName not in responses:
+		responses[responseUserName] = sentMsg
+
+	posts.find_one_and_update({"id":postId}, {'$set':{"respondedPeople": responses}})
+	newResponse = {"respondPeople": responseUserName, "respondMessage": sentMsg, "postID":postId}
+	responds.insert_one(newResponse)
+
+	return
+
+
+@app.route("/messages", methods=["GET", "POST"])
+def addNewPost(restaurantName, postUserName, time):
+	newID = postUserName + " " + restaurantName + " " + time
+	newPost = {"sender":postUserName, "restaurant":restaurantName, "time":time, "respondedPeople":[], "id": newID}
+	posts.insert_one(newPost)
+	return 
+
 
 if __name__ == '__main__':
 	client = pymongo.MongoClient('mongodb://localhost:27017/')
